a3a8dff24a4dca62981e2be607f7be6f7fa1acb4
Yiannis Hadjicharalambous <Yiannis.Hadjicharalambous@netbuilder.com>
2019-11-04 21:17:26 +0000

    Improvements

--- annoy-scammers.py
+++ annoy-scammers.py
@@ -3,16 +3,20 @@ import random
 import string
 import json
 
-chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+chars = 3*string.ascii_letters + string.digits + '!@#$%^&*()'
 
 url = 'https://secure-update.authveri-fynetflix.net/ajax/submit.php' # replace appropriately
+
+emails = ["@gmail.com","@hotmail.com","@outlook.com"]
+
+digits = [x for x in range(100)]
 
 names = json.loads(open('names.json').read())
 
 for name in names:
-	name_extra = ''.join(random.choice(string.digits))
+	name_extra = str(random.choice(digits))
 
-	username = name.lower() + name_extra + '@gmail.com'
+	username = name.lower() + name_extra + random.choice(emails)
 	password = ''.join(random.choice(chars) for i in range(10))
 
 	requests.post(url, allow_redirects=False, data={
