ab5e28c85023d0351dc2267acd123c50b62dea00
Yiannis Hadjicharalambous <Yiannis.Hadjicharalambous@netbuilder.com>
2019-12-04 20:06:04 +0000

    Order

---
+++ Coding-tasks/RestAPIsTask.py
@@ -0,0 +1,36 @@
+import requests
+
+class RESTApp():
+      
+    def __init__(self, URL):
+      self.url = URL
+
+    def getAllUsers(self):
+      self.url = "https://jsonplaceholder.typicode.com/users"
+      r = requests.get(self.url)
+      return r.json()
+
+    def getAllPosts(self):
+      self.url = "https://jsonplaceholder.typicode.com/posts"
+      r = requests.get(self.url)
+      return r.json()
+
+    def getUserDetails(self, username):
+      for user in self.getAllUsers():
+        if user['username'] == username:
+          return user
+
+    def getAllUserPosts(self, username):
+      for user in self.getAllUsers():
+        if user['username'] == username:
+          id = user['id']
+          break
+      for post in self.getAllPosts():
+        if id == post['userId']:
+          print(post)
+
+    def getCommentsTotal(self, username):
+      return None
+        
+if __name__ == '__main__':
+    test = RESTApp("https://jsonplaceholder.typicode.com")
