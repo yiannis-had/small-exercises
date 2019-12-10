2ebd6b8414ea0e556a14343bb1e612a62f649700
Yiannis Hadjicharalambous <johngate10@hotmail.com>
2019-12-10 14:41:23 +0000

    Fix

---
+++ Exercises/guess_number.py
@@ -0,0 +1,19 @@
+import random	
+secretNumber = random.randint(1, 20)	
+print('I am thinking of a number between 1 and 20.')	
+
+for guessesTaken in range(1, 6):	
+    print('Take a guess.')	
+    guess = int(input())	
+
+    if guess < secretNumber:	
+        print('Your guess is too low.')	
+    elif guess > secretNumber:	
+        print('Your guess is too high.')	
+    else:	
+        break
+
+if guess == secretNumber:	
+    print('You guessed my number in ' + str(guessesTaken) + ' guesses!')	
+else:	
+    print('The number I was thinking of was ' + str(secretNumber))
