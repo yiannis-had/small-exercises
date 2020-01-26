d74584b4978b48d661fba5d014bbf4b2cfc3f061
Yiannis Hadjicharalambous <johngate10@hotmail.com>
2020-01-26 21:39:54 +0000

    PEP8

--- Requests/amazon.py
+++ Requests/amazon.py
@@ -4,9 +4,10 @@ import win32.win32api
 import re
 import requests
 import pyperclip
+from selenium import webdriver
 
 
-url = "https://chipmonkzslots.com/offers"
+url = " "
 
 sleeptime = 1.25
 
@@ -16,27 +17,31 @@ ignore = [
     "AQBQ-S2DAAC-M3KBP",
     "AQGF-GSY77H-LXSB6",
     "AQ6W-NMPZK3-72SB9",
+    "AQ2Z-AYB46G-KFNBZ",
     "AQWE-CM87M9-3ZABL",
     "AQGM-G4W9DR-5SHBS",
 ]
 
 
-def getHash():
+def getregex():
 
     r = requests.get(url)
     text = r.text
     global regex
     regex = re.findall("[A-Z0-9]{4,6}\-[A-Z0-9]{4,6}\-[A-Z0-9]{4,6}", text)
-    print(regex)
-    return hashlib.sha256(text.encode("utf-8")).hexdigest()
+    return regex
 
 
-current_hash = getHash()
+browser = webdriver.Edge()
+browser.get("https://www.amazon.co.uk/gc/redeem")
+giftcard = browser.find_element_by_id("gc-redemption-input")
 
 while 1:
-    if getHash() == current_hash or regex == ignore:
-        print("Not Changed")
+    if getregex() == ignore:
+        print(regex)
     else:
+        giftcard.send_keys(regex[0])
+        giftcard.submit()
         pyperclip.copy(regex[0])
         win32.win32api.MessageBox(0, "CHANGED", "CHANGED", 0x00001000)
         print("Changed")
