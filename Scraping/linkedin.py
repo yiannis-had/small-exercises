0aeeef7f0ca44facfb4917e0d9b318e83f296fd6
Yiannis Hadjicharalambous <johngate10@hotmail.com>
2020-02-25 16:11:26 +0200

    PEP8

---
+++ Scraping/linkedin.py
@@ -0,0 +1,22 @@
+from linkedin import linkedin
+
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+USER_TOKEN = ""
+USER_SECRET = ""
+RETURN_URL = "http://www.google.com"
+
+auth = linkedin.LinkedInDeveloperAuthentication(
+    CONSUMER_KEY,
+    CONSUMER_SECRET,
+    USER_TOKEN,
+    USER_SECRET,
+    RETURN_URL,
+    permissions=linkedin.PERMISSIONS.enums.values(),
+)
+
+app = linkedin.LinkedInApplication(auth)
+
+app.get_profile()
+
+connections = app.get_connections()
